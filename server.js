c993254113f71b013fb74f82d9bb987629ec1fe5
Subrata Sarker <subrata_sarker@yahoo.com>
2019-03-19 00:43:20 +0900

    Fix favicon route

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.resolve(__dirname, 'public/favicon.ico')));
 app.use(express.static(__dirname));
 
-app.get('*', (req, res, next) => {
+app.get('/', (req, res, next) => {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
